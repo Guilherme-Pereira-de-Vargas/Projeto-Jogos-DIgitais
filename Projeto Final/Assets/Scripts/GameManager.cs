58f0b0548290048953b6971cc383f55d7f1c1d2d
Guilherme Pereira de Vargas <guilhermepvargas79@gmail.com>
2025-11-04 16:05:17 -0300

    animacoes, ponto e vida

--- Projeto Final/Assets/Scripts/GameManager.cs
+++ Projeto Final/Assets/Scripts/GameManager.cs
@@ -7,22 +7,30 @@ public class GameManager : MonoBehaviour
     public int vidas = 3;
     public TextMeshProUGUI textPontos;
 
+    private void Start()
+    {
+        InvokeRepeating("AddPoints", 0.01f, 3f);
+    }
+
+    // Método chamado automaticamente pelo InvokeRepeating
+    private void AddPoints()
+    {
+        AddPoints(10); // adiciona, por exemplo, 10 pontos a cada 3 segundos
+    }
+
     public void AddPoints(int qtd)
     {
-        pontos = pontos + qtd;
+        pontos += qtd;
 
-        if (pontos <= 0)
-        {
+        if (pontos < 0)
             pontos = 0;
-        }
 
-        textPontos.text = "Pontos: " + pontos;
         Debug.Log("Pontos: " + pontos);
     }
 
     public void LostLifes(int vida)
     {
-        vidas = vidas - vida;
+        vidas -= vida;
         Debug.Log("Vidas: " + vidas);
 
         GameObject player = GameObject.FindWithTag("Player");
